058a4c8482e4acf7d61a3bded517bf9fe5a2d23a
Pierrot <pierr.lescure+git@gmail.com>
2024-12-27 17:39:18 +0100

    Modify long press on keyboard

--- autorun.sh
+++ autorun.sh
@@ -10,5 +10,6 @@ run "rhythmbox"
 run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
 run "numlockx"
 run "dropbox"
-setxkbmap fr oss -option caps:escape
 run "unclutter"
+setxkbmap fr oss -option caps:escape
+xset r rate 300 25
