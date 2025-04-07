7ee61ee04a57a2e9c202448b67a4188de0af5cd8
Pierrot <pierr.lescure+git@gmail.com>
2025-04-07 21:44:01 +0200

    Add 3 tags

--- rc_laptop.lua
+++ rc_laptop.lua
@@ -388,7 +388,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[2])
+    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "°", "+" }, s, awful.layout.layouts[2])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
@@ -677,13 +677,47 @@ clientkeys = gears.table.join(
         {description = 'toggle title bar', group = 'client'})
 )
 
+
+globalkeys = gears.table.join(globalkeys,
+    awful.key({ modkey }, "à",
+              function ()
+                    local screen = awful.screen.focused()
+                    local tag = screen.tags[10]
+                    if tag then
+                       tag:view_only()
+                    end
+              end,
+              {description = "view tag #0", group = "tag"}),
+    awful.key({ modkey }, ")",
+              function ()
+                    local screen = awful.screen.focused()
+                    local tag = screen.tags[11]
+                    if tag then
+                       tag:view_only()
+                    end
+              end,
+              {description = "view tag #°", group = "tag"}),
+    awful.key({ modkey }, "=",
+              function ()
+                    local screen = awful.screen.focused()
+                    local tag = screen.tags[12]
+                    if tag then
+                       tag:view_only()
+                    end
+              end,
+              {description = "view tag #+", group = "tag"})
+          )
+
 -- Bind all key numbers to tags.
 -- Be careful: we use keycodes to make it work on any keyboard layout.
 -- This should map on the top row of your keyboard, usually 1 to 9.
-for i = 1, 9 do
+
+tag_list_print = {'1','2','3','4','5','6','7','8','9','0','°','+'}
+tag_list_key   = {'&','é','"',"'",'(','-','è','_','ç','à',')','='}
+for i = 1, 12 do
     globalkeys = gears.table.join(globalkeys,
         -- View tag only.
-        awful.key({ modkey }, "#" .. i + 9,
+        awful.key({ modkey }, tag_list_key[i],
                   function ()
                         local screen = awful.screen.focused()
                         local tag = screen.tags[i]
@@ -691,9 +725,9 @@ for i = 1, 9 do
                            tag:view_only()
                         end
                   end,
-                  {description = "view tag #"..i, group = "tag"}),
+                  {description = "view tag #"..tag_list_print[i], group = "tag"}),
         -- Toggle tag display.
-        awful.key({ modkey, "Control" }, "#" .. i + 9,
+        awful.key({ modkey, "Control" }, tag_list_key[i],
                   function ()
                       local screen = awful.screen.focused()
                       local tag = screen.tags[i]
@@ -701,9 +735,9 @@ for i = 1, 9 do
                          awful.tag.viewtoggle(tag)
                       end
                   end,
-                  {description = "toggle tag #" .. i, group = "tag"}),
+                  {description = "toggle tag #" .. tag_list_print[i], group = "tag"}),
         -- Move client to tag.
-        awful.key({ modkey, "Shift" }, "#" .. i + 9,
+        awful.key({ modkey, "Shift" }, tag_list_key[i],
                   function ()
                       if client.focus then
                           local tag = client.focus.screen.tags[i]
@@ -712,9 +746,9 @@ for i = 1, 9 do
                           end
                      end
                   end,
-                  {description = "move focused client to tag #"..i, group = "tag"}),
+                  {description = "move focused client to tag #"..tag_list_print[i], group = "tag"}),
         -- Toggle tag on focused client.
-        awful.key({ modkey, "Control", "Shift" }, "#" .. i + 9,
+        awful.key({ modkey, "Control", "Shift" }, tag_list_key[i],
                   function ()
                       if client.focus then
                           local tag = client.focus.screen.tags[i]
@@ -723,7 +757,7 @@ for i = 1, 9 do
                           end
                       end
                   end,
-                  {description = "toggle focused client on tag #" .. i, group = "tag"})
+                  {description = "toggle focused client on tag #" .. tag_list_print[i], group = "tag"})
     )
 end
 
